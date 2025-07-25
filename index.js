bd082e8cc7caaec1b79b89e0ef524d1b19c8a0b8
R-Srijanki <jankirathod999@gmail.com>
2025-07-25 20:42:16 +0530

    afteraddingcssandtailwindcss

--- index.js
+++ index.js
@@ -10,29 +10,64 @@ const day4=document.getElementById("day4");
 const day5=document.getElementById("day5");
 const api="e7a563e8d10e65b9e7de4f87d478bb54"; 
 const errormsg=document.getElementById("error-msg");
-
+const check=document.getElementById("toggle");
+const page=document.getElementById("main");
+const ball=document.getElementById("ball");
+const currhead=document.getElementById("currhead");
+const forehead=document.getElementById("forehead");
 
 let lat,lon;
 search.addEventListener("click",find);
+track.addEventListener("click",getLocation);
+
+
+check.addEventListener("change",function(){
+    if(check.checked){
+        page.style.backgroundImage="url('./images/pexels-pixabay-355887.jpg')";
+        ball.style.transform="translateX(22px)";
+        currhead.style.color="white";
+        forehead.style.color="white";
+    }
+    else{
+        page.style.backgroundImage="url('./images/field-with-clouds.jpg')";
+        ball.style.transform="translateX(0)";
+        currhead.style.color="black";
+        forehead.style.color="black";
+    }
+})
+
+
+
 function showError(message) {
     const errorBox = document.getElementById("error-box");
     const errorMsg = document.getElementById("error-msg");
 
-    errorMsg.innerText = message;
     errorBox.classList.remove("hidden");
+    errorMsg.innerText = message;
+    
 }
 
 function hideErrorBox() {
    const errorBox = document.getElementById("error-box");
    const errorMsg=document.getElementById("error-msg");
-if (errorBox) {
+    if (errorBox) {
     errorBox.classList.add("hidden");if (errorMsg) errorMsg.innerText = "";
+    }
 }
-} 
+
 async function find(){
     hideErrorBox();
-    if (!cityname.value.trim()) {
+    const city=cityname.value.trim();
+    if (!city) {
     showError("Please enter a city name");
+    return;
+    }
+    if(city.length<2){
+        showError("Please enter valid city name");
+    return;
+    }
+    if(!/^[a-zA-Z\s]+$/.test(city)){
+        showError("Please enter letters only");
     return;
     }
     try{
@@ -46,6 +81,7 @@ async function find(){
         lon=locdata[0].lon;
         
         history(cityname.value);
+        
         currcard();
         cityname.value="";
     }
@@ -54,7 +90,7 @@ async function find(){
         showError("An error occurred while fetching location");
     }
 }
-track.addEventListener("click",getLocation);
+
 
 his.addEventListener("change", async function () {
     hideErrorBox();
@@ -85,14 +121,21 @@ function getLocation(){
     else
         showError("Geolocation is not supported by this browser.");
 }
-function success(position){
+
+
+async function success(position){
     lat=position.coords.latitude;
     lon=position.coords.longitude;
     
-    history(getCityNameFromCoords(lat, lon));
-    currcard();
-    cityname.value="";
+    const city=await getCityNameFromCoords(lat, lon);
+    if (city) {
+    history(city);     // Assuming 'history' is your function to store/show city
+    currcard();        // Your function to show current weather
+    cityname.value = "";
+  }
 }
+
+
 function error(error){
     switch(error.code){
         case error.PERMISSION_DENIED:
@@ -111,10 +154,19 @@ function error(error){
 }
 
 async function getCityNameFromCoords(lat, lon) {
-  const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${api}`;
-  const res = await fetch(url);
+  try{
+  const res = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${api}`);
+  if (!res.ok) {
+      throw new Error("Failed to fetch location");
+    }
   const data = await res.json();
-  return data.length ? data[0].name : null;   // e.g. "Mumbai"
+  return data.length ? data[0].name : null;  // e.g. "Mumbai" 
+  }
+  catch(error){
+    showError("An error occurred while fetching location name.");
+    return null;
+  }
+   
 }
 
 async function currcard(){
@@ -130,6 +182,8 @@ async function currcard(){
         const humidity=document.createElement("span");
         const wind_speed=document.createElement("span");
         const img=document.createElement("img");
+        const store=document.createElement("div");
+        store.classList.add("currcard");
         // first converts to milliseconds and then Human-readable date
         const readabledate = new Date(currweatherdata.dt * 1000).toLocaleDateString();
 
@@ -144,7 +198,10 @@ async function currcard(){
        //clear previous content
         pre.innerHTML = "";
         //Append to container
-        pre.append(date,temp,humidity,wind_speed,img);
+        
+        store.append(date,temp,humidity,wind_speed);
+        currhead.classList.remove("hidden");
+        pre.append(store,img);
         cards();
     }
     catch(error){
@@ -168,7 +225,9 @@ async function cards(){
             const humidity=document.createElement("span");
             const wind_speed=document.createElement("span");
             const img=document.createElement("img");
-
+            const store=document.createElement("div");
+            store.classList.add("forecards");
+            
             const readabledate = new Date(element.dt * 1000).toLocaleDateString();
 
             date.innerText=`Date: ${readabledate}`;
@@ -179,26 +238,27 @@ async function cards(){
             const iconcode= element.weather[0].icon;
             img.src=`https://openweathermap.org/img/wn/${iconcode}@2x.png`;
             img.alt="Weather icon";
-
+            store.append(date,temp,humidity,wind_speed);
+            forehead.classList.remove("hidden");
             switch(index){
                 case 0:{
                     day1.innerHTML = "";
-                    day1.append(date,temp,humidity,wind_speed,img); break;}
+                    day1.append(store,img); break;}
                 case 1:{
                     day2.innerHTML = "";
-                    day2.append(date,temp,humidity,wind_speed,img); break;}
+                    day2.append(store,img); break;}
                 case 2:{
                     day3.innerHTML = "";
-                    day3.append(date,temp,humidity,wind_speed,img); break;}
+                    day3.append(store,img); break;}
                 case 3:{
                     day4.innerHTML = "";
-                    day4.append(date,temp,humidity,wind_speed,img); break;}
+                    day4.append(store,img); break;}
                 case 4:{
                     day5.innerHTML = "";
-                    day5.append(date,temp,humidity,wind_speed,img); break;}
+                    day5.append(store,img); break;}
             }
         });
-
+        
 
     }
     catch(error){
@@ -208,27 +268,24 @@ async function cards(){
 
 function history(cityname){
     const citytext=cityname.trim();
+    let limit=5;
     if(!citytext) return;
     let cities=JSON.parse(localStorage.getItem("cities"))||[];
     
-    const exist=cities.some(function(c){
-        return(c.cityname.toLowerCase()===citytext.toLowerCase());
+    cities=cities.filter(function(c){
+        return(c.cityname.toLowerCase()!==citytext.toLowerCase());
     });
-    if(!exist){
-        const city={
+     const city={
             cityname: citytext,
         };
-        cities.push(city);
-        localStorage.setItem("cities",JSON.stringify(cities));
-        const opt=document.createElement("option");
-        opt.innerText=citytext;
-        opt.value=citytext;
-        his.append(opt);
-    }
-
+    cities.unshift(city);
+    if(cities.length>limit)
+        cities=cities.slice(0,limit);
+    localStorage.setItem("cities",JSON.stringify(cities));
+    updatedropdown();
 }
-
-document.addEventListener("DOMContentLoaded", () => {
+function updatedropdown(){
+     his.innerHTML = '<option value="">Select a previous city</option>'; 
     const cities=JSON.parse(localStorage.getItem("cities")) || [];
     cities.forEach((element)=>{
         const opt=document.createElement("option");
@@ -236,5 +293,8 @@ document.addEventListener("DOMContentLoaded", () => {
         opt.value=element.cityname;
         his.append(opt);
     });
+}
+document.addEventListener("DOMContentLoaded", () => {
+   updatedropdown();
 });
 
